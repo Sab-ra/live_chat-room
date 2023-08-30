9f4e6c0e14460410fe07abc9d7fcccfc5bf8f4c2
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-30 08:21:24 +0300

    Add global styles to project

--- src/App.vue
+++ src/App.vue
@@ -13,5 +13,9 @@ export default {
 </script>
 
 <style>
-
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
