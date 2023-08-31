2b87d6cad2e191719c269083fc5cce071aaa7c15
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-31 21:47:47 +0300

    Draft signup form

---
+++ src/components/SignUp.vue
@@ -0,0 +1,33 @@
+<template>
+  <form @submit.prevent="handleSignupInput">
+    <input type="text" required placeholder="First name" v-model="displayName">
+    <input type="email" required placeholder="your@email.hear" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <button>
+      Sign Up         
+    </button>
+  </form>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    //refs
+    let displayName = ref( '' )
+    let email = ref( '' )
+    let password = ref( '' )
+
+    const handleSignupInput = () => {
+      console.log( displayName.value, email.value, password.value )
+    }
+
+    return { displayName, email, password, handleSignupInput }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <SignUpVue/>
   </div>
 </template>
 
 <script>
+import SignUpVue from '@/components/SignUp.vue'
+
 export default {
+
+  components: {
+    SignUpVue
+  }
 
 }
 </script>
