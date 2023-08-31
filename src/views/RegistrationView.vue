663f6fe75f7d0fdcc65143aede0e1efaf4f82652
Sab-ra <119448985+Sab-ra@users.noreply.github.com>
2023-08-31 22:07:49 +0300

    Style registration view

--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -25,4 +25,25 @@ export default {
     text-align: center;
     padding: 20px 0px;
   }
+
+  .welcome form {
+    width: 80%;
+    max-width: 300px;
+    margin: 20px auto;
+  }
+
+  .welcome label {
+    display: block;
+    margin: 20px 0px 10px;
+  }
+
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
 </style>
